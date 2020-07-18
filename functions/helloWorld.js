1a456af2eba76660e7792cb89d961fbf9447eab0
ncaudill27 <nelson.a.caudill@gmail.com>
2020-07-18 18:18:12 -0400

    Nest event and context in callback response to see on screen

--- functions/helloWorld.js
+++ functions/helloWorld.js
@@ -1,9 +1,17 @@
 exports.handler = function(event, context, callback) {
-  console.log("Event:", event);
-  console.log("Context:", context);
+  // console.log("Event:", event);
+  // console.log("Context:", context);
 
   return callback(null, {
     statusCode: 200,
-    body: 'Hello, World"'
+    body: JSON.stringify({
+      text: 'Hello, World',
+      attachments: [
+        {
+          event: {...event},
+          context: {...context}
+        }
+      ]
+    })
   });
 };
