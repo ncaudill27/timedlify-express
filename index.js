770b958367d60aa88700e84a158d12597daa9562
ncaudill27 <nelson.a.caudill@gmail.com>
2020-07-20 12:35:39 -0400

    Switch back to working with the widget

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-import GoTrue from 'gotrue-js';
-import handleLogin from './helpers/handleLogin.js';
+// import GoTrue from 'gotrue-js';
+// import handleLogin from './helpers/handleLogin.js';
 
-const isLocal = document.location.host.split(':').shift() === 'localhost';
+// const isLocal = document.location.host.split(':').shift() === 'localhost';
 
-export const auth = new GoTrue({
-  APIUrl: 'https://timedlify.netlify.app/.netlify/identity',
-  setCookie: !isLocal
-})
+// export const auth = new GoTrue({
+//   APIUrl: 'https://timedlify.netlify.app/.netlify/identity',
+//   setCookie: !isLocal
+// })
 
-const loginForm = document.getElementById('login');
-loginForm.addEventListener('submit', handleLogin);
-console.log('mounted');
+// const loginForm = document.getElementById('login');
+// loginForm.addEventListener('submit', handleLogin);
+// console.log('mounted');
